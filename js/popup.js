8eb9d630aeb13c693b269b3c7ef381d006ec6db0
Sam Gavis-Hughson <sgavis@princeton.edu>
2014-06-15 12:55:55 -0400

    copy to clipboard button

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,12 @@
+function copyTextToClipboard(text) {
+    var copyFrom = $('<textarea/>');
+    copyFrom.text(text);
+    $('body').append(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    copyFrom.remove();
+}
+
 $(function() {
 	$('#chirp-button').click(function() {
 		url = 'http://chirpappapi-env-kxv8gngpvg.elasticbeanstalk.com/chirpapp/api/v1.0/' 
@@ -14,6 +23,9 @@ $(function() {
 	$('#tweet-button').click(function() {
 		var newURL = "https://twitter.com/intent/tweet?text=" + encodeURIComponent($('#tweet').val());
   		chrome.tabs.create({ url: newURL });
+	});
+	$('#copy-button').click(function() {
+		copyTextToClipboard($('#tweet').val())
 	})
 });
 
